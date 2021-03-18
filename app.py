3364211b51062039f95717a4603af5ed66648d29
Unn4m3DD <guerreiroapatacas@gmail.com>
2021-03-18 16:06:17 +0000

    account for hand distance in closed metric

--- app.py
+++ app.py
@@ -30,6 +30,7 @@ with mp_hands.Hands(
     if results.multi_hand_landmarks:
       for hand_landmarks in results.multi_hand_landmarks:
         index_finger_insertion = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP]
+        middle_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
         pinky_finger_insertion = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP]
         ring_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP]
         wrist = hand_landmarks.landmark[mp_hands.HandLandmark.WRIST]
@@ -42,16 +43,30 @@ with mp_hands.Hands(
             )
           )
         ))
+
         closed = sqrt(
               (ring_finger_tip.x - wrist.x) ** 2 + 
               (ring_finger_tip.y - wrist.y) ** 2 + 
               (ring_finger_tip.z - wrist.z) ** 2
+            ) / sqrt(
+              (pinky_finger_insertion.x - index_finger_insertion.x) ** 2 + 
+              (pinky_finger_insertion.y - index_finger_insertion.y) ** 2
+            )
+        
+        direction = (
+          middle_finger_tip.x - wrist.x,
+          middle_finger_tip.y - wrist.y
         )
-        print(f"                            ", end="\r")
-        print(f"~{angle:.2f}º, closed metric {closed}", end="\r")
+        print(f"                                         ", end="\r")
+        print(f"~{angle:.2f}º, closed metric {closed}, direction ({direction[0]}, {direction[1]})", end="\r")
         mp_drawing.draw_landmarks(
             image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
     cv2.imshow('MediaPipe Hands', image)
     if cv2.waitKey(5) & 0xFF == 27:
       break
 cap.release()
+
+
+#  #   ###
+# ###  ###
+#  #   ###
